65ec721a3bacea810bbd1619e0a7ce1c32d1d390
Dmitry Medychenko <rAv3ndima91@gmail.com>
2020-11-03 23:41:41 +0200

    заканчивает работу над colorSwitcher

--- src/task.js
+++ src/task.js
@@ -10,3 +10,31 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const startBtnRef = document.querySelector('[data-action="start"]');
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+const bodyRef = document.querySelector('body');
+
+startBtnRef.addEventListener('click', onColorSwitch);
+let switcherTimerId = null;
+
+function onColorSwitch() {
+  if (!startBtnRef.disabled) {
+    switcherTimerId = setInterval(() => {
+      bodyRef.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+    startBtnRef.disabled = true;
+    stopBtnRef.disabled = false;
+  }
+}
+
+stopBtnRef.addEventListener('click', onStopColorSwitch);
+
+function onStopColorSwitch() {
+  if (!stopBtnRef.disabled) {
+    clearInterval(switcherTimerId);
+    startBtnRef.disabled = false;
+    stopBtnRef.disabled = true;
+  }
+}
